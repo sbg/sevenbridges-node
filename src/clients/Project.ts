93787bd2a102773b7e2e60d63aee08d6523d2ee6
Dragan Bajcic <dragan@bajcic.com>
2016-03-21 15:53:59 -0400

    fix gulp file so it can recover WATCH after build step crashed during lint / some other step.

--- src/clients/Project.ts
+++ src/clients/Project.ts
@@ -1,13 +1,47 @@
 import {Base} from './Base';
 
 export class Project extends Base {
+
     sessionId: string;
 
     constructor() {
         super();
-        
+
         this.sessionId = '';
     }
 
+
+    list() {
+        console.log('TBD');
+    }
+
+    createNew() {
+        console.log('TBD');
+    }
+
+    listByUserName(username: string) {
+        console.log('TBD');
+    }
+
+
+    delete() {
+        console.log('TBD');
+    }
+
+    getDetails() {
+        console.log('TBD');
+    }
+
+    edit() {
+        console.log('TBD');
+    }
+
+    listFiles() {
+        console.log('TBD');
+    }
+
+    listMembers() {
+        console.log('TBD');
+    }
 }
 
