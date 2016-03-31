087754498ec33806ff175a9943affa36db271e52
Filip Jelic <jelic.filip@gmail.com>
2016-03-31 16:11:55 +0200

    instantiate file client

--- src/interfaces/ClientInterfaces.ts
+++ src/interfaces/ClientInterfaces.ts
@@ -1,11 +1,13 @@
 import {BillingInterfaces} from './BillingInterfaces';
 import {ProjectInterfaces} from './ProjectInterfaces';
 import {UserInterfaces} from './UserInterfaces';
+import {FileInterfaces} from './FileInterfaces';
 
 export namespace ClientInterfaces {
 
     export interface BillingClientInterface extends BillingInterfaces.BillingClientInterface {}
     export interface ProjectClientInterface extends ProjectInterfaces.ProjectClientInterface {}
     export interface UserClientInterface extends UserInterfaces.UserClientInterface {}
+    export interface FileClientInterface extends FileInterfaces.FileClientInterface {}
 
 }
--- src/sbg.ts
+++ src/sbg.ts
@@ -3,12 +3,14 @@ import {ClientInterfaces} from './interfaces/ClientInterfaces';
 import {Project} from './clients/Project';
 import {Billing} from './clients/Billing';
 import {User} from './clients/User';
+import {File} from './clients/File';
 
 class SBG {
 
     Projects : ClientInterfaces.ProjectClientInterface;
     Billing : ClientInterfaces.BillingClientInterface;
     User : ClientInterfaces.UserClientInterface;
+    File : ClientInterfaces.FileClientInterface;
 
     constructor() {
         this.instantiateClients();
@@ -18,6 +20,7 @@ class SBG {
         this.Projects = new Project();
         this.Billing = new Billing();
         this.User = new User();
+        this.File = new File();
     }
 
 }
